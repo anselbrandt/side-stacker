6fe057dafa99b64e0391a2ad5e5f4c3901aa51ec
anselbrandt <ansel@anselbrandt.com>
2025-03-09 13:19:28 -0700

    Remove websocket cookie

--- backend/app/main.py
+++ backend/app/main.py
@@ -191,12 +191,11 @@ connections = {}
 
 async def get_token(
     websocket: WebSocket,
-    session: Annotated[str | None, Cookie()] = None,
     token: Annotated[str | None, Query()] = None,
 ):
-    if session is None and token is None:
+    if token is None:
         raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
-    return session or token
+    return token
 
 
 @app.websocket("/ws/{user_id}")
