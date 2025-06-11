5f975a9fe90f0c49255ecf12fe73ac7198488f8d
anselbrandt <ansel@anselbrandt.com>
2025-06-11 02:21:58 -0400

    Remove Vite

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["air.anselbrandt.net"],
+  output: 'export',
+  distDir: 'dist',
 };
 
 export default nextConfig;
